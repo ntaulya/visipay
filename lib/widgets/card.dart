07aa2c7ced676cbe74e5d46e98d34c877f5fba57
ILHAM ILYASA <64563404+mahli262@users.noreply.github.com>
2023-06-11 11:47:14 +0700

    card button

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -23,7 +23,7 @@ class CardButton extends StatelessWidget {
         ),
         child: InkWell(
           onTap: () {
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: route));
+            Navigator.push(context, MaterialPageRoute(builder: route));
           },
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
