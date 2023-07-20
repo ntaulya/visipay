74babe647c30dbc031bcee6d09e8856510c7e454
foxxxesky <abdurrahman1467@gmail.com>
2023-07-21 00:03:38 +0700

    Update register_event.dart

--- lib/bloc/register/register_event.dart
+++ lib/bloc/register/register_event.dart
@@ -3,11 +3,17 @@ part of 'register_bloc.dart';
 @immutable
 abstract class RegisterEvent {}
 
-class RegisterFormSubmit extends RegisterEvent{
+class RegisterFormSubmit extends RegisterEvent {
   final String phone;
   final String name;
   final String email;
   final String security_code;
 
   RegisterFormSubmit(this.phone, this.name, this.email, this.security_code);
+}
+
+class finduser extends RegisterEvent {
+  final String phone;
+
+  finduser(this.phone);
 }
