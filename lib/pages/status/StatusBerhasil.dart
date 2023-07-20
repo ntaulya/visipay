3eff13f264a9d36a0a8a9c9a58228046f1ce1a4a
foxxxesky <abdurrahman1467@gmail.com>
2023-07-20 21:57:34 +0700

    Update StatusBerhasil.dart

--- lib/pages/status/StatusBerhasil.dart
+++ lib/pages/status/StatusBerhasil.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:visipay/core/theme/palette.dart';
@@ -22,37 +20,44 @@ class _StatusBerhasilState extends State<StatusBerhasil> {
     super.initState();
     Timer(Duration(seconds: 3), () {
       // Navigator.of(context).pushReplacementNamed('/onboarding');
-      Navigator.push(context, 
-      MaterialPageRoute(builder: (context) => Home(),)
-      );
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Home(),
+          ));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+            child: Scaffold(
           body: Container(
-            decoration: const BoxDecoration(image: DecorationImage(
-              image: AssetImage("assets/img/Wallpaper2.png"),
-              fit: BoxFit.cover
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/img/Wallpaper2.png"), fit: BoxFit.cover),
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 308,
+                  ),
+                  Image.asset(
+                    "assets/img/Completed successfully.png",
+                    width: 313,
+                    height: 313,
+                  ),
+                  // Image.asset("assets/img/Payment_failed.png",width: 313,height: 313,),
+                  Text("Transaksi Berhasil",
+                      style: GoogleFonts.nunito(
+                          textStyle: Nunito_25px, fontWeight: FontWeight.w600, color: Text1)),
+                ],
+              ),
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 308,),
-              Image.asset("assets/img/Completed successfully.png",width: 313,height: 313,),
-              // Image.asset("assets/img/Payment_failed.png",width: 313,height: 313,),
-              Text("Transaksi Berhasil", style: GoogleFonts.nunito(textStyle: Nunito_25px,fontWeight: FontWeight.w600,color: Text1 )),
-            ],
-          ),
-        ),
-      ),
-      )
-      )
-      );
+        )));
   }
 }
