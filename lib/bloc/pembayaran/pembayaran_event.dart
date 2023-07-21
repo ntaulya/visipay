389485266e14f9d278fed5e84fcc8dd539c1309a
foxxxesky <abdurrahman1467@gmail.com>
2023-07-21 15:40:40 +0700

    Update pembayaran_event.dart

--- lib/bloc/pembayaran/pembayaran_event.dart
+++ lib/bloc/pembayaran/pembayaran_event.dart
@@ -6,7 +6,8 @@ class PembayaranInisiate extends PembayaranEvent {
   final String product_id;
   final String promo_id;
   final String notes;
+  final String? billing_number;
 
-  PembayaranInisiate({required this.product_id, required this.promo_id, required this.notes});
+  PembayaranInisiate(
+      {required this.product_id, required this.promo_id, required this.notes, this.billing_number});
 }
-
