52c1380c6fc92056ad3e91986fd9a3fc24e903d1
foxxxesky <abdurrahman1467@gmail.com>
2023-07-21 22:28:41 +0700

    Update login_event.dart

--- lib/bloc/login/login_event.dart
+++ lib/bloc/login/login_event.dart
@@ -3,15 +3,21 @@ part of 'login_bloc.dart';
 @immutable
 abstract class LoginEvent {}
 
-class LoginFormSubmit extends LoginEvent{
+class LoginFormSubmit extends LoginEvent {
   final String phone;
   final String security_code;
 
   LoginFormSubmit(this.phone, this.security_code);
 }
 
-class finduser extends LoginEvent{
+class finduser extends LoginEvent {
   final String phone;
 
   finduser(this.phone);
+}
+
+class userPIN extends LoginEvent {
+  final String security_code;
+
+  userPIN(this.security_code);
 }
